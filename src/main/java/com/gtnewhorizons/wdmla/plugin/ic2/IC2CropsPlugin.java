24c4885e709c0b7ab96475acefe8680e54d42c8e
chicken7201 <cmsboo1482@gmail.com>
2026-08-02 22:12:57 +0900

    Add excluded dependencies for conditional plugins

--- src/main/java/com/gtnewhorizons/wdmla/plugin/ic2/IC2CropsPlugin.java
+++ src/main/java/com/gtnewhorizons/wdmla/plugin/ic2/IC2CropsPlugin.java
@@ -10,7 +10,7 @@ import com.gtnewhorizons.wdmla.api.WDMlaPlugin;
 import ic2.api.crops.ICropTile;
 
 /** Absorbs the IC2 crop-card integration from WAILAPlugins. */
-@WDMlaPlugin(uid = "ic2_crops", dependencies = "IC2")
+@WDMlaPlugin(uid = "ic2_crops", dependencies = "IC2", excludedDependencies = "cropsnh")
 public class IC2CropsPlugin implements IWDMlaPlugin {
 
     /** Registers the conditional IC2 crop renderer for blocks. */
