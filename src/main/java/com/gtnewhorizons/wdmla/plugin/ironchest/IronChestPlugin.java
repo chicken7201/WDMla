3c28b43607ffb8a44874a8af1ac47358a014b954
chicken7201 <cmsboo1482@gmail.com>
2026-08-04 11:14:12 +0900

    Fix IronChest plugin dependency identifier

--- src/main/java/com/gtnewhorizons/wdmla/plugin/ironchest/IronChestPlugin.java
+++ src/main/java/com/gtnewhorizons/wdmla/plugin/ironchest/IronChestPlugin.java
@@ -7,9 +7,10 @@ import com.gtnewhorizons.wdmla.plugin.universal.ItemStorageProvider;
 
 import cpw.mods.ironchest.BlockIronChest;
 
-@WDMlaPlugin(uid = "ironchest", dependencies = "ironchest")
+@WDMlaPlugin(uid = "ironchest", dependencies = "IronChest")
 public class IronChestPlugin implements IWDMlaPlugin {
 
+    /** Registers Iron Chests blocks with the same item-storage preview used by vanilla chests. */
     @Override
     public void register(IWDMlaCommonRegistration registration) {
         registration.registerItemStorage(ItemStorageProvider.Extension.INSTANCE, BlockIronChest.class);
